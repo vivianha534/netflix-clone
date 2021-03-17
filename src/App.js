c3081f2f7a8a370630bed59777df1ce493f8822b
Vivian Ha <vivian.ha534@gmail.com>
2021-03-17 00:00:27 -0700

    Finished the top banner

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from "react"
-import Row from "./components/Row/Row.js"
-import requests from './requests'
+import RowsPage from "./container/Rows/RowsPage.js"
+import Banner from "./components/Banner/Banner.js"
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Banner/>
+      <RowsPage/>
     </div>
   );
 }
