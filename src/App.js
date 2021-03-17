2e6512fd42a196bb520688f93c00c407e647f8f0
Vivian Ha <vivian.ha534@gmail.com>
2021-03-17 01:00:17 -0700

    finished navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import RowsPage from "./container/Rows/RowsPage.js"
 import Banner from "./components/Banner/Banner.js"
+import NavBar from "./components/NavBar/NavBar.js"
 import "./App.css"
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Banner/>
       <RowsPage/>
     </div>
